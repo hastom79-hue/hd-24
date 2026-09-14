891a368a18ffa96fb1fa768b648535ff3b852aa1
hastom79-hue <hastom79@gmail.com>
2026-09-14 11:02:04 +0900

    feat: load follow-up mail preview and reply history module

--- hd24-ui-v3.js
+++ hd24-ui-v3.js
@@ -1,1 +1,1 @@
-document.write('<script src="./hd24-ui-v3-core.js?v=18"><\/script><script src="./hd24-auto-run.js?v=19"><\/script>');
+document.write('<script src="./hd24-ui-v3-core.js?v=18"><\/script><script src="./hd24-auto-run.js?v=19"><\/script><script src="./hd24-followup.js?v=20"><\/script>');
